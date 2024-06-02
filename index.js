79feb0eb50ecb6ca3e3852ca412ffbfe56403d92
Luvkush_Sharma <yos8760@gmail.com>
2024-06-02 11:43:08 +0530

    Initial Code Added

---
+++ index.js
@@ -0,0 +1,7 @@
+const age = 21;
+const name = 'Luvkush';
+const dob = '22-07-2004';
+
+function sayName() {
+  console.log(`My name is ${name}`);
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+function sayHello() {
+  console.log('Hello');
+}
+
+// Exporting the function sayHello
